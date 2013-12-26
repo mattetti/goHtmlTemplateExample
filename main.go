c5c804122b2e11aba499b6b920f8dd1c2ea7d289
Matt Aimonetti <mattaimonetti@gmail.com>
2013-12-26 12:36:26 -0800

    added comments

--- main.go
+++ main.go
@@ -9,10 +9,15 @@ import (
 )
 
 var templates *template.Template
+
+// custom template delimiters since the Go default delimiters clash
+// with Angular's default.
 var templateDelims = []string{"{{%", "%}}"}
 
 func init() {
-	// initialize the templates
+	// initialize the templates,
+	// couldn't have used http://golang.org/pkg/html/template/#ParseGlob
+	// since we have custom delimiters.
 	basePath := "resources/templates/"
 	err := filepath.Walk(basePath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -39,9 +44,13 @@ func init() {
 }
 
 func main() {
+	// serve static assets showing how to strip/change the path.
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("resources/images"))))
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("resources/javascripts"))))
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("resources/stylesheets"))))
+	// home page handler, defined in handlers.go
 	http.HandleFunc("/", homeHandler)
+	// start the server on port 1313
+	// go to http://localhost:1313 to see the rendered content.
 	http.ListenAndServe(":1313", nil)
 }
